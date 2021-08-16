9d8663829204222d7f757e64132d1362088cb5e2
c-artist <andilemkhizekhabazela@gmail.com>
2021-08-16 11:27:19 +0200

    updated task 6 logic

--- 6-task.js
+++ 6-task.js
@@ -1,11 +1,13 @@
 function getMax(a, b, c) {
   let arr = [];
-  let highestNum = 0;
+  let highestNum = null;
   for (let val of arguments) {
     arr.push(val);
   }
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > highestNum) {
+    if (highestNum === null) {
+      highestNum = arr[i];
+    } else if (arr[i] > highestNum) {
       highestNum = arr[i];
     }
   }
