5deb797d20df03785a778e2bcaeb742c5426d47d
c-artist <andilemkhizekhabazela@gmail.com>
2021-08-16 11:52:58 +0200

    apdated plurals

--- 8-task.js
+++ 8-task.js
@@ -1,12 +1,12 @@
 function convertToHoursAndMin(num) {
   let isOver = num > 59,
     min = num % 60,
-    minStatement = min > 1 ? `${min} minutes` : `${min} minute`;
+    minStatement = min === 1 ? `${min} minute` : `${min} minutes`;
   if (isOver) {
     let hours = Math.trunc(num / 60),
       hourStatement = hours > 1 ? `${hours} hours` : `${hours} hour`;
-    return console.log(hourStatement, minStatement);
+    console.log(hourStatement, minStatement);
   } else {
-    return console.log(`0 hour`, minStatement);
+    console.log(`0 hours`, minStatement);
   }
 }
