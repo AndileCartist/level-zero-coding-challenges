b731886cb47b5e3ac6ab5a3bc24ac1d062b1c54e
AndileCartist <36767197+AndileCartist@users.noreply.github.com>
2021-09-14 18:53:54 +0200

    updated task 8

--- 8-task.js
+++ 8-task.js
@@ -1,9 +1,9 @@
 function convertToHoursAndMin(num) {
-  let isOver = num > 59,
+  const isOver = num > 59,
     min = num % 60,
     minStatement = min === 1 ? `${min} minute` : `${min} minutes`;
   if (isOver) {
-    let hours = Math.trunc(num / 60),
+    const hours = Math.trunc(num / 60),
       hourStatement = hours > 1 ? `${hours} hours` : `${hours} hour`;
     console.log(hourStatement, minStatement);
   } else {
